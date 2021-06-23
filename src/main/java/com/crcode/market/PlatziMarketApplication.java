0129a73a6b028bc92e8d0ccf06693625f92e797b
Cristian Alejandro Idarraga <idarragacristian8@gmail.com>
2021-06-23 14:15:48 -0500

    first commit

---
+++ src/main/java/com/crcode/market/PlatziMarketApplication.java
@@ -0,0 +1,13 @@
+package com.crcode.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication	// Para indicar que esta clase gestiona la aplicacion
+public class PlatziMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatziMarketApplication.class, args);
+	}
+
+}
